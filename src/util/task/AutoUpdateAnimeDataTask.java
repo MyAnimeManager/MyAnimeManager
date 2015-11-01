a0500caa59048f6075d7c5d4fbb408b15f6a99b5
iTTou <perotti.denis@gmail.com>
2015-11-01 02:07:52 +0100

    bug fix

--- src/util/task/AutoUpdateAnimeDataTask.java
+++ src/util/task/AutoUpdateAnimeDataTask.java
@@ -41,7 +41,13 @@ public class AutoUpdateAnimeDataTask extends SwingWorker
 				String data = ConnectionManager.parseAnimeData(id);
 				
 				String totalEp = ConnectionManager.getAnimeData("total_episodes", data);
+				if(totalEp.equals("null")||totalEp.equals("0"))
+					totalEp = "??";
 				String duration = ConnectionManager.getAnimeData("duration", data);
+				if(duration.equals("null")||duration.equals("0"))
+					duration = "?? min";
+				else
+					duration += " min";
 				String startDate = ConnectionManager.getAnimeData("start_date", data);
 				if (startDate.equals("null"))
 					startDate = "??/??/????";
@@ -133,14 +139,14 @@ public class AutoUpdateAnimeDataTask extends SwingWorker
 				boolean[] exclusionArray = AnimeIndex.exclusionAnime.get(name);
 				String data = ConnectionManager.parseAnimeData(id);
 				
-				if(!exclusionArray[1])
+				if(exclusionArray[1]==false)
 				{
 					totalEp = ConnectionManager.getAnimeData("total_episodes", data);
 					if(totalEp.equals("null")||totalEp.equals("0"))
 						totalEp = "??";
 				}
 				
-				if(!exclusionArray[2])
+				if(exclusionArray[2]==false)
 				{
 					 duration = ConnectionManager.getAnimeData("duration", data);
 					if(duration.equals("null")||duration.equals("0"))
@@ -149,7 +155,7 @@ public class AutoUpdateAnimeDataTask extends SwingWorker
 						duration += " min";
 				}
 				
-				if (!exclusionArray[3])
+				if (exclusionArray[3]==false)
 				{
 					startDate = ConnectionManager.getAnimeData("start_date", data);
 					
@@ -172,7 +178,7 @@ public class AutoUpdateAnimeDataTask extends SwingWorker
 					}
 				}
 				
-				if(!exclusionArray[4])
+				if(exclusionArray[4]==false)
 				{
 					finishDate = ConnectionManager.getAnimeData("end_date", data);
 					
@@ -194,15 +200,41 @@ public class AutoUpdateAnimeDataTask extends SwingWorker
 						finishDate = dayEnd + "/" + monthEnd + "/" + yearEnd;
 					}
 					if (totalEp.equals("1"))
-						finishDate = startDate;
-				}
+					{
+						if (exclusionArray[3]==false)
+							finishDate = startDate;
+						else
+						{
+							startDate = ConnectionManager.getAnimeData("start_date", data);
+							
+							if (startDate.equals("null"))
+								startDate = "??/??/????";
+							else if(startDate.length()==4)
+								startDate = "??/??/" + startDate;
+							else if(startDate.length()==7)
+							{
+								String monthStart = startDate.substring(5, 7);
+								String yearStart = startDate.substring(0, 4);
+								startDate = "??/" + monthStart + "/" + yearStart;
+							}
+							else if (startDate.length() > 7)
+							{
+								String dayStart = startDate.substring(8, 10);
+								String monthStart = startDate.substring(5, 7);
+								String yearStart = startDate.substring(0, 4);
+								startDate = dayStart + "/" + monthStart + "/" + yearStart;
+							}
+							finishDate = startDate;
+						}
+					}
+				}	
 				
-				if(exclusionArray[5] && !type.equalsIgnoreCase("Blu-ray"))
+				if(exclusionArray[5]==false && !type.equalsIgnoreCase("Blu-ray"))
 				{
 					type = ConnectionManager.getAnimeData("type", data);
 				}
 
-				if (AnimeIndex.appProp.getProperty("excludeImage").equalsIgnoreCase("false") && !exclusionArray[0])
+				if (exclusionArray[0]==false)
 				{
 					String imageLink = ConnectionManager.getAnimeData("image_url_lge", data);
 					imageLink = imageLink.replaceAll("\\\\/", "/");
--- src/util/task/ManualUpdateAnimeDataTask.java
+++ src/util/task/ManualUpdateAnimeDataTask.java
@@ -110,7 +110,31 @@ public class ManualUpdateAnimeDataTask extends SwingWorker
 					finishDate = dayEnd + "/" + monthEnd + "/" + yearEnd;
 				}
 				if (totalEp.equals("1"))
+				{	
+					if (exclusionArray[3]==false)
+					{
+						startDate = ConnectionManager.getAnimeData("start_date", data);
+						
+						if (startDate.equals("null"))
+							startDate = "??/??/????";
+						else if(startDate.length()==4)
+							startDate = "??/??/" + startDate;
+						else if(startDate.length()==7)
+						{
+							String monthStart = startDate.substring(5, 7);
+							String yearStart = startDate.substring(0, 4);
+							startDate = "??/" + monthStart + "/" + yearStart;
+						}
+						else if (startDate.length() > 7)
+						{
+							String dayStart = startDate.substring(8, 10);
+							String monthStart = startDate.substring(5, 7);
+							String yearStart = startDate.substring(0, 4);
+							startDate = dayStart + "/" + monthStart + "/" + yearStart;
+						}
+					}
 					finishDate = startDate;
+				}
 			}
 			
 			if(exclusionArray[5])
