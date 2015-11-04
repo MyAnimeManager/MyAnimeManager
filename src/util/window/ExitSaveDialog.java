769db6aaf2a29255ca2521d971f541c94e847eb1
iTTou <perotti.denis@gmail.com>
2015-11-04 20:58:07 +0100

    bug fix

--- src/util/window/ExitSaveDialog.java
+++ src/util/window/ExitSaveDialog.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.TreeMap;
 
 import javax.swing.JButton;
