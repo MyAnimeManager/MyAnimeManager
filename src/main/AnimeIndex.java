19b17709c15d99ab20130c490efb72e6370667e4
Samu <Samu@192.168.1.2>
2015-11-08 16:56:15 +0100

    BugFix

--- src/main/AnimeIndex.java
+++ src/main/AnimeIndex.java
@@ -92,7 +92,7 @@ import util.window.WishlistDialog;
 
 public class AnimeIndex extends JFrame
 {
-	public static final String VERSION = "1.0.4 beta";
+	public static final String VERSION = "1.0.0";
 	public static final String CURRENT_VERSION = "MyAnimeManager.exe";
 	public static final String NEW_VERSION = "MyAnimeManager_Setup.exe";
 	public static JPanel mainFrame;
