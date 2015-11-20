0288da5717eea999369480c5cd06a8a68bb99367
Samu <Samu@192.168.1.2>
2015-11-20 16:58:42 +0100

    Rimossi import

--- src/util/window/MusicDialog.java
+++ src/util/window/MusicDialog.java
@@ -1,25 +1,20 @@
 package util.window;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
 import javax.swing.border.EmptyBorder;
 
 import util.MAMUtil;
-
-import java.awt.Font;
-import java.awt.Dialog.ModalityType;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.JScrollPane;
-import java.awt.Insets;
-import javax.swing.JTree;
-import javax.swing.JLabel;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JProgressBar;
 
 
 public class MusicDialog extends JDialog {
