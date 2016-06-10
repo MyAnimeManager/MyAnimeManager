ddaf99ca511182e265a66c22d63e3cbd5a21f750
Samu <Samu@Samu-PC.homenet.telecomitalia.it>
2016-06-10 19:47:15 +0200

    Dialog sincronizzazione da MAM

---
+++ src/util/window/SynchronizingDialog.java
@@ -0,0 +1,79 @@
+package util.window;
+
+import java.awt.Cursor;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+
+import net.miginfocom.swing.MigLayout;
+import util.task.MALSynchronizationTask;
+
+public class SynchronizingDialog extends JDialog
+{
+
+	private JProgressBar progressBar;
+	private JLabel lblDownloadInCorso;
+	MALSynchronizationTask task;
+
+	/**
+	 * Create the dialog.
+	 */	
+	public SynchronizingDialog(String username)
+	{
+		task = new MALSynchronizationTask(username);
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowOpened(WindowEvent e)
+			{
+				setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+				task.execute();
+			}
+		});
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		setModal(true);
+		setResizable(false);
+		setTitle("Sincronizzazione dati da MyAnimeList");
+		setBounds(100, 100, 328, 85);
+
+		getContentPane().setLayout(new MigLayout("", "[320.00px]", "[14px][14px]"));
+		lblDownloadInCorso = new JLabel("Inizio sincronizzazione...");
+		lblDownloadInCorso.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblDownloadInCorso, "cell 0 0,growx,aligny center");
+
+		progressBar = new JProgressBar();
+		progressBar.setIndeterminate(true);
+		getContentPane().add(progressBar, "cell 0 1,growx,aligny center");
+
+		task.addPropertyChangeListener(new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt)
+			{
+				
+				if (evt.getPropertyName().equals("progress"))
+				{
+					if (progressBar.isIndeterminate())
+					{
+						progressBar.setIndeterminate(false);
+						progressBar.setMinimum(0);
+						progressBar.setMaximum((int)task.totalAnimeNumber);
+					}
+					progressBar.setValue((int)task.currentAnimeNumber);
+				}
+				else if (evt.getPropertyName().equals("state"))
+					if (evt.getNewValue().toString().equalsIgnoreCase("done"))
+						SynchronizingDialog.this.dispose();
+
+			}
+		});
+
+	}
+}
