78e8fada6cf37eb4d4fe920dc2414a07df8c5517
Samu <Samu@192.168.1.2>
2016-07-14 17:04:34 +0200

    BugFix Export a MAL

--- src/util/task/SynchronizeToMALTask.java
+++ src/util/task/SynchronizeToMALTask.java
@@ -1,5 +1,6 @@
 package util.task;
 
+import java.awt.BorderLayout;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -7,13 +8,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingWorker;
 
 import main.AnimeIndex;
 import util.AnimeData;
 import util.ConnectionManager;
 import util.MAMUtil;
+import util.SortedListModel;
 
 
 public class SynchronizeToMALTask extends SwingWorker
@@ -23,8 +29,10 @@ public class SynchronizeToMALTask extends SwingWorker
 	private List animeToAdd;
 	private ArrayList<String> conflictedAnime = new ArrayList<String>();
 	private ArrayList<String> notFoundedAnime = new ArrayList<String>();
+	private ArrayList<String> animeNotSynchronized = new ArrayList<String>();
 	public float totalAnimeNumber;
 	public float currentAnimeNumber;
+	public String currentAnime;
 	boolean credentialOk;
 	
 	public SynchronizeToMALTask(String username, String password, List animeToAdd)
@@ -41,110 +49,134 @@ public class SynchronizeToMALTask extends SwingWorker
 		try
 		{
 			credentialOk = ConnectionManager.verifyCredentialsMAL(username, password);
+			if (credentialOk)
+			{
+				setProgress(0);	
+				synchronizeAnimeToMAL(animeToAdd, true);
+				synchronizeAnimeToMAL(notFoundedAnime, false);
+			}
 		}
 		catch (IOException e)
 		{
 			MAMUtil.writeLog(e);
 			e.printStackTrace();
 		}
-		if (credentialOk)
-		{
-			setProgress(0);	
-			synchronizeAnimeToMAL(animeToAdd, true);
-			synchronizeAnimeToMAL(notFoundedAnime, false);
-		}
 		return null;
 	}
 	
 	protected void done()
 	{
-		if (credentialOk)
+		try
 		{
-			int size = conflictedAnime.size();
-			int current = 1;
-			for (String anime : conflictedAnime)
+			if (credentialOk)
 			{
-				HashMap<String, Integer> map = ConnectionManager.getAnimeSearchedMAL(username, password, anime);
-				if (map.size() > 1)
+				int size = conflictedAnime.size();
+				int current = 1;
+				for (String anime : conflictedAnime)
 				{
-					String[] animeArray = map.keySet().toArray(new String[0]);
-					String animeName = (String) JOptionPane.showInputDialog(AnimeIndex.frame, "Scegli l'anime da aggiungere per \"" + anime + "\" :", "Conflitto trovato" + current + "/" + size, JOptionPane.QUESTION_MESSAGE, null, animeArray, animeArray[0]);
-					if (animeName != null)
+					currentAnime = anime;
+					HashMap<String, Integer> map = ConnectionManager.getAnimeSearchedMAL(username, password, anime);
+					if (map.size() > 1)
 					{
-						int id = map.get(animeName);
-						String episode = "";
-						String status = "";
-						String comments = "";
-						if (AnimeIndex.completedMap.containsKey(anime))
+						String[] animeArray = map.keySet().toArray(new String[0]);
+						String animeName = (String) JOptionPane.showInputDialog(AnimeIndex.frame, "Scegli l'anime da aggiungere per \"" + anime + "\" :", "Conflitto trovato" + current + "/" + size, JOptionPane.QUESTION_MESSAGE, null, animeArray, animeArray[0]);
+						if (animeName != null)
 						{
-							AnimeData data = AnimeIndex.completedMap.get(anime);
-							episode = data.getCurrentEpisode();
-							status = "2";
-							comments = data.getNote();
-						}
-						else if (AnimeIndex.airingMap.containsKey(anime))
-						{
-							AnimeData data = AnimeIndex.airingMap.get(anime);
-							episode = data.getCurrentEpisode();
-							status = "1";
-							comments = data.getNote();
-						}
-						else if (AnimeIndex.ovaMap.containsKey(anime))
-						{
-							AnimeData data = AnimeIndex.ovaMap.get(anime);
-							episode = data.getCurrentEpisode();
-							String episodeTotal = data.getTotalEpisode();
-							if (episode.equals(episodeTotal))
+							int id = map.get(animeName);
+							String episode = "";
+							String status = "";
+							String comments = "";
+							if (AnimeIndex.completedMap.containsKey(anime))
+							{
+								AnimeData data = AnimeIndex.completedMap.get(anime);
+								episode = data.getCurrentEpisode();
 								status = "2";
-							else
+								comments = data.getNote();
+							}
+							else if (AnimeIndex.airingMap.containsKey(anime))
+							{
+								AnimeData data = AnimeIndex.airingMap.get(anime);
+								episode = data.getCurrentEpisode();
 								status = "1";
-							comments = data.getNote();
-						}
-						else if (AnimeIndex.filmMap.containsKey(anime))
-						{
-							AnimeData data = AnimeIndex.completedMap.get(anime);
-							episode = data.getCurrentEpisode();
-							
-							String today = MAMUtil.today();
-							GregorianCalendar todayDate = MAMUtil.getDate(today);
-							String release = data.getReleaseDate();
-							GregorianCalendar releaseDate = MAMUtil.getDate(release);
-							if (releaseDate.before(todayDate) || releaseDate.equals(todayDate))
+								comments = data.getNote();
+							}
+							else if (AnimeIndex.ovaMap.containsKey(anime))
+							{
+								AnimeData data = AnimeIndex.ovaMap.get(anime);
+								episode = data.getCurrentEpisode();
+								String episodeTotal = data.getTotalEpisode();
+								if (episode.equals(episodeTotal))
+									status = "2";
+								else
+									status = "1";
+								comments = data.getNote();
+							}
+							else if (AnimeIndex.filmMap.containsKey(anime))
+							{
+								AnimeData data = AnimeIndex.completedMap.get(anime);
+								episode = data.getCurrentEpisode();
+								
+								String today = MAMUtil.today();
+								GregorianCalendar todayDate = MAMUtil.getDate(today);
+								String release = data.getReleaseDate();
+								GregorianCalendar releaseDate = MAMUtil.getDate(release);
+								if (releaseDate.before(todayDate) || releaseDate.equals(todayDate))
+									status = "1";
+								else
+									status = "6";
+								comments = data.getNote();
+							}
+							else if (AnimeIndex.completedToSeeMap.containsKey(anime))
+							{
+								AnimeData data = AnimeIndex.completedToSeeMap.get(anime);
+								episode = data.getCurrentEpisode();
 								status = "1";
-							else
-								status = "6";
-							comments = data.getNote();
-						}
-						else if (AnimeIndex.completedToSeeMap.containsKey(anime))
-						{
-							AnimeData data = AnimeIndex.completedToSeeMap.get(anime);
-							episode = data.getCurrentEpisode();
-							status = "1";
-							comments = data.getNote();
-						}
-						try
-						{
-							ConnectionManager.addAnimeMAL(username, password, id, episode, status, comments);
-							current++;
-						}
-						catch (IOException e)
-						{
-							MAMUtil.writeLog(e);
-							e.printStackTrace();
-						}
-					}				
-				}
-			} 
+								comments = data.getNote();
+							}
+							try
+							{
+								ConnectionManager.addAnimeMAL(username, password, id, episode, status, comments);
+								current++;
+							}
+							catch (IOException e)
+							{
+								MAMUtil.writeLog(e);
+								e.printStackTrace();
+							}
+						}				
+					}
+				} 
+			
+			if (animeNotSynchronized.size() > 0)
+			{
+				JPanel panel = new JPanel(new BorderLayout(5, 5));
+				panel.add(new JLabel("I seguenti anime non sono stati trovati:"), BorderLayout.NORTH);
+				JScrollPane scroll = new JScrollPane();
+				JList list = new JList();
+				SortedListModel model = new SortedListModel();
+				list.setModel(model);
+				model.addAll(animeNotSynchronized.toArray(new String[0]));
+				scroll.setViewportView(list);
+				panel.add(scroll, BorderLayout.CENTER);
+				JOptionPane.showMessageDialog(AnimeIndex.frame, panel, "Anime non trovati!", JOptionPane.INFORMATION_MESSAGE);
+			}
+			}
+			else
+				JOptionPane.showMessageDialog(AnimeIndex.frame, "Errore di Connessione o Credenziali Errate!", "Errore!", JOptionPane.ERROR_MESSAGE);
 		}
-		else
-			JOptionPane.showMessageDialog(AnimeIndex.frame, "Credenziali Errate!", "Errore!", JOptionPane.ERROR_MESSAGE);
+		catch (Exception e)
+		{
+			MAMUtil.writeLog(e);
+			e.printStackTrace();
+		}
 	}
 	
 	private void synchronizeAnimeToMAL(List<String> animeToAdd, boolean shouldAddToNotFounded)
 	{
 		for (Object obj : animeToAdd)
-		{		
+		{	
 			String animeName = (String) obj;
+			currentAnime = animeName;
 			HashMap<String, Integer> map = ConnectionManager.getAnimeSearchedMAL(username, password, animeName);
 			if (map.size() == 1)
 			{
@@ -315,6 +347,7 @@ public class SynchronizeToMALTask extends SwingWorker
 					notFoundedAnime.add(animeName.replace(": ", ":"));
 				else
 				{
+					animeNotSynchronized.add(animeName);
 					currentAnimeNumber++;
 				}
 			}
