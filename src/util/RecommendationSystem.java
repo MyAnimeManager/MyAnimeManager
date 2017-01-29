64edfd21eb5a2b1e9c0f01c36c35dd864a06a9f2
Cristiano Colpo <cristianocolpo@gmail.com>
2017-01-29 21:16:15 +0100

    Aggiunto ReccomendationSystem

---
+++ src/util/RecommendationSystem.java
@@ -0,0 +1,12 @@
+package util;
+
+public class RecommendationSystem {
+	private String username;
+	public RecommendationSystem(String username) {
+		// TODO Auto-generated constructor stub
+		this.username = username;
+	}
+	public boolean esiste() {
+		return false;
+	}
+}
