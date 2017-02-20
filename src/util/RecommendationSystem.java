7c50460bb4540d9804fb3f9a18b98306d2b6a009
Cristiano Colpo <cristianocolpo@gmail.com>
2017-02-20 19:29:10 +0100

    Aggiunte modifiche al provaJson

--- src/util/RecommendationSystem.java
+++ src/util/RecommendationSystem.java
@@ -5,17 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 
 public class RecommendationSystem {
+	public static void main(String[] args) {
+		RecommendationSystem prova = new RecommendationSystem("SirSeleucio");
+		ArrayList prove = prova.provaJson();
+		System.out.println(prove.size());
+	}
 	private String username;
 	public RecommendationSystem(String username) {
 		// TODO Auto-generated constructor stub
@@ -51,13 +58,25 @@ public class RecommendationSystem {
 		{
 			
 		}
-		result = StringEscapeUtils.unescapeJava(result);
 		return result;
 	}
-	public String banana() {
+	public ArrayList provaJson() {
 		JsonParser parser = new JsonParser();
-	      JsonElement animeList = parser.parse(request());
-	      JsonObject anime = animeList.getAsJsonObject();
-	      return null;
+		  JsonElement animeList = parser.parse(request());
+		  JsonObject anime = animeList.getAsJsonObject();
+		  JsonArray Data = anime.getAsJsonArray("Data");
+		  JsonElement PearsonEle = Data.get(0);
+		  JsonObject dataObj = PearsonEle.getAsJsonObject();
+		  JsonArray PearsonArr = dataObj.getAsJsonArray("Pearson");
+		  ArrayList<Integer> ids = new ArrayList<Integer>();
+		  for (JsonElement jsonElement : PearsonArr) {
+			JsonObject obj = jsonElement.getAsJsonObject();
+			Double pearson = obj.get("Pearson").getAsDouble();
+			if (pearson > 0.3) {
+				int id = obj.get("id").getAsInt();
+					ids.add(id);
+			}
+		}
+	      return ids;
 	}
 }
