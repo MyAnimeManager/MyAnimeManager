a78cbd674a0f95e07b58541f1341b59c2078f962
yesod <yesod@Samu-Fisso>
2017-10-28 14:26:33 +0200

    Revert

--- src/util/ConnectionManager.java
+++ src/util/ConnectionManager.java
@@ -678,38 +678,6 @@ public class ConnectionManager
 		conn.setRequestProperty("User-Agent", "My Anime Manager");
 		conn.setRequestProperty("Content-Type", "application/json");
 		conn.setRequestProperty("Accept", "application/json");
-		
-		 TrustManager[] trustAllCerts = new TrustManager[] {
-			       new X509TrustManager() {
-			          public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-			            return null;
-			          }
-			          @Override
-			          public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
-
-			          @Override
-			          public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
-						
-					}
-
-			    };
-
-			    SSLContext sc = SSLContext.getInstance("SSL");
-			    sc.init(null, trustAllCerts, new java.security.SecureRandom());
-			    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-			    // Create all-trusting host name verifier
-			    HostnameVerifier allHostsValid = new HostnameVerifier() {
-			        public boolean verify(String hostname, SSLSession session) {
-			          return true;
-			        }
-			    };
-			    // Install the all-trusting host verifier
-			    HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-			    /*
-			     * end of the fix
-			     */
-		
 		return conn;
 	}
 	
